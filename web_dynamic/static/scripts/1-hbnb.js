394338af0f15a524ee7d6b3ee42e3d7ae7429f0e
arturadamian <arturadamian@gmail.com>
2018-09-11 01:59:34 +0000

    works well but should fix css

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,14 @@
 window.onload = function () {
   $(':checkbox').css('margin-right', '10px');
   let listAmenity = [];
+
   $('input:checkbox').change(function () {
     if (this.checked) {
-      listAmenity.push($(this).attr('data-name'));
+      listAmenity.push(this.dataset.name);
+      $.each(listAmenity, function (index, element) {
+      });
     } else {
-      listAmenity.pop($(this).attr('data-name'));
+      listAmenity.splice(listAmenity.indexOf(this.dataset.name), 1);
     }
     $('.amenities h4').text(listAmenity.join(', '));
   });
