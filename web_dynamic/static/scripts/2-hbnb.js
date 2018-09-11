b8295b6f5c6e513b8e3e9840a796926356f82757
adentintime <arixbizz@gmail.com>
2018-09-11 08:01:23 +0000

    Update 2-3, semistandard corrections

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,9 +18,9 @@ window.onload = function () {
   });
 };
 $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
-	if (data.status === 'OK') {
-		$('#api_status').addClass('available');
-	} else {
-		$('#api_status').removeClass('available');
-	}
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
 });
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -18,47 +18,47 @@ window.onload = function () {
   });
 };
 $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
-	if (data.status === 'OK') {
-		$('#api_status').addClass('available');
-	} else {
-		$('#api_status').removeClass('available');
-	}
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
 });
 $.ajax({
-	type: 'POST',
-	url: 'http://0.0.0.0:5001/api/v1/places_search/',
-	data: '{}',
-	contentType: 'application/json',
-	success: (returnedData) => {
-		returnedData.forEach((place) => {
-			$('SECTION.places').append(
-                                        '<article>' +
-                                          '<div class="title">' +
-                                            '<h2>' + place.name + '</h2>' +
-                                            '<div class="price_by_night">' + place.price_by_night + '</div>' +
-                                          '</div>' +
-                                          '<div class="information">' +
-                                            '<div class="max_guest">' +
-                                              '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
-                                              '<br />' +
-                                              place.max_guest + (place.max_guest > 1 || place.max_guest == 0? ' Guests' : ' Guest') +
-                                            '</div>' +
-                                            '<div class="number_rooms">' +
-                                              '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
-                                              '<br />' +
-                                              place.number_rooms + (place.number_rooms > 1 || place.number_rooms == 0 ? ' Bedrooms' : ' Bedroom') +
-                                            '</div>' +
-                                            '<div class="number_bathrooms">' +
-                                              '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
-                                              '<br />' +
-                                              place.number_bathrooms + (place.number_bathrooms > 1 || place.number_bathrooms == 0 ? ' Bathrooms' : ' Bathroom') +
-                                            '</div>' +
-                                          '</div>' +
-                                          '<div class="description">' +
-                                            place.description +
-                                          '</div>' +
-                                        '</article>'
-                                        );
-		});
-	},
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  data: '{}',
+  contentType: 'application/json',
+  success: (returnedData) => {
+    returnedData.forEach((place) => {
+      $('SECTION.places').append(
+        '<article>' +
+          '<div class="title">' +
+            '<h2>' + place.name + '</h2>' +
+            '<div class="price_by_night">' + place.price_by_night + '</div>' +
+          '</div>' +
+          '<div class="information">' +
+            '<div class="max_guest">' +
+              '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+              '<br />' +
+              place.max_guest + (place.max_guest > 1 || place.max_guest === 0 ? ' Guests' : ' Guest') +
+            '</div>' +
+            '<div class="number_rooms">' +
+              '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+              '<br />' +
+              place.number_rooms + (place.number_rooms > 1 || place.number_rooms === 0 ? ' Bedrooms' : ' Bedroom') +
+            '</div>' +
+            '<div class="number_bathrooms">' +
+              '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+              '<br />' +
+              place.number_bathrooms + (place.number_bathrooms > 1 || place.number_bathrooms === 0 ? ' Bathrooms' : ' Bathroom') +
+            '</div>' +
+          '</div>' +
+          '<div class="description">' +
+            place.description +
+          '</div>' +
+        '</article>'
+      );
+    });
+  }
 });
